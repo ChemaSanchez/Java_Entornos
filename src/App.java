3975737fd7ba8eaa784fb79898487cd29e696d0d
Juanmi <jferval807@g.educaand.es>
2022-03-15 21:02:08 +0100

    Este es el cambio 2 de juanmi

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Klk");
+        System.out.println("Chema es un inútil");
     }
 }
