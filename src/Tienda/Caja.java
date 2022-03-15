419f99579b357015227cc3b50e6b6d7946bf9b8c
ChemaSanchez11 <josemaria.chauchina@gmail.com>
2022-03-15 21:15:13 +0100

    Arreglar Cambios Mario

---
+++ src/Tienda/Caja.java
@@ -0,0 +1,16 @@
+package Tienda;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Chema
+ */
+public class Caja{
+    
+    private int billetes50;
+
+    public CarritoCompra() {
+        carrito = new ArrayList<>();
+    }
+}
