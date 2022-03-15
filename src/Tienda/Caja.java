5451485a0662d0a698a46429670356ba88c7ca02
admin <droidermunoz@gmail.com>
2022-03-15 21:14:28 +0100

    He añadido propiedades

---
+++ src/Tienda/Caja.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Mario
+ */
+public class Caja{
+    
+    private int billetes50;
+    private int billetes20;
+    private int billetes10;
+    private int billetes5;
+    private double monedas;
+
+    public Caja() {
+        billetes50=0;
+        billetes20=0;
+        billetes10=0;
+        billetes5=0;
+        monedas=0;
+    }
+}
