fb3039e2c6d86960d22d19363833e9a761539ab8
admin <droidermunoz@gmail.com>
2022-03-17 17:53:37 +0100

    Para vosotros, jugadores

--- src/Tienda/Caja.java
+++ src/Tienda/Caja.java
@@ -20,4 +20,28 @@ public class Caja{
         billetes5=0;
         monedas=0;
     }
+    public String pago(int d){
+        resto=0;
+        dinero=d;
+        if(d%50 >0){
+            billetes50=dinero/50;
+            resto=dinero%50;
+        }
+        if(resto%20>0){
+            billetes20=resto/20;
+            resto=resto%20;
+        }
+        if(resto%10>0){
+            billetes10=resto/10;
+            resto=resto%10;
+        }
+        if(resto%5>0){
+            billetes5=resto/5;
+            resto=resto%5;
+        }
+        if(resto%2>0){
+            monedas=resto;
+        }
+        return "La caja tiene "+billetes50+" billetes de 50, "+billetes20+" de 20, "+billetes10+" de 10, "+billetes5+" de 5 y "+monedas+" euros" 
+    }
 }
