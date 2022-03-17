48664b9b6fa7f5ac20cc41ba0697ed69ddf08d46
ChemaSanchez <josemaria.chauchina@gmail.com>
2022-03-17 14:01:10 +0100

    Metodo esRTX

--- src/Tienda/TarjetaGrafica.java
+++ src/Tienda/TarjetaGrafica.java
@@ -4,5 +4,11 @@ public class TarjetaGrafica extends Componentes{
     public TarjetaGrafica(String n, double p) {
         super(n,p);
     }
-    
+
+    public boolean esRTX(){
+        if (getNombre().startsWith("RTX")){
+            return true;
+        }
+        else return false;
+    }
 }
